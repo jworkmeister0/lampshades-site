db66be07a217601bf12260d73a7d7b83585d617f
jworkmeister0 <jworkmeister@zoho.com>
2016-12-10 13:28:21 -0500

    its a new day :)

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,103 +1,109 @@
 (function(){
 
-  $(function(){
-    var $pageList = $("#pages");
+	$(function(){
+		var $pageList = $("#pages");
 
-    checkUrlHash();
-    applyRandomness();
-    applyEventListeners();
+		checkUrlHash();
+		applyRandomness();
+		applyEventListeners();
 
-    function applyRandomness(){
-      var navHoverRand = makeRandomizer(1, 9);
-      var jumboRando = makeRandomizer(7, 9);
+		function applyRandomness(){
+			var navHoverRand = makeRandomizer(1, 9);
+			var jumboRando = makeRandomizer(1, 3);
 
-      $(".jumbotron").addClass(jumboRando.getNextRandClass());
+			$(".jumbotron").addClass(jumboRando.getNextJumbotronClass());
 
-      $.each($("#main-nav").children(), function(){
-        $(this).addClass(navHoverRand.getNextRandClass());
-      });
-    }
+			$.each($("#main-nav").children(), function(){
+				$(this).addClass(navHoverRand.getNextNavClass());
+			});
+		}
 
-    function checkUrlHash(){
-      if (!window.location.hash){
-        showPage("news");
-      }
-      else{
-        showPage(window.location.hash);
-      }
-    }
+		function checkUrlHash(){
+			if (!window.location.hash){
+				showPage("news");
+			}
+			else{
+				showPage(window.location.hash);
+			}
+		}
 
-    function applyEventListeners(){
-      window.onhashchange = function(e){
-        showPage(window.location.hash);
-      };
+		function applyEventListeners(){
+			window.onhashchange = function(e){
+				showPage(window.location.hash);
+			};
 
-    }
+			$(".jumbotron").click(function(){
+				$("body").toggleClass("inverted");
+			});
+		}
 
-    function showPage(pageName){
-      var pageId = pageName.split("#").join("") + "-page";
-      $pageList.children("div.page").each(function(){
-        if($(this).attr("id") === pageId){
-          $(this).fadeIn();
-          $(this).addClass("active");
-        } else{
-          $(this).hide();
-          $(this).removeClass("active");
-        }
-      });
-    }
-  });
+		function showPage(pageName){
+			var pageId = pageName.split("#").join("") + "-page";
+			$pageList.children("div.page").each(function(){
+				if($(this).attr("id") === pageId){
+					$(this).fadeIn();
+					$(this).addClass("active");
+				} else{
+					$(this).hide();
+					$(this).removeClass("active");
+				}
+			});
+		}
+	});
 
 
-  function makeRandomizer(min, max){
-    var arr = [];
-    var index = 0;
-    var className = "nav-rand-hover-";
+	function makeRandomizer(min, max){
+		var arr = [];
+		var index = 0;
+		var CLASS_NAMES = {
+			navHover: "nav-rand-hover-",
+			jumbo: "header-rand-"
+		};
 
-    for (var i = min; i <= max; i++){
-      arr.push(i);
-    }
+		for (var i = min; i <= max; i++){
+			arr.push(i);
+		}
 
-    arr = shuffle(arr);
+		arr = shuffle(arr);
 
-    return {
-      getNextRand: function(){
-        index += 1;
-        if (index >= arr.length){
-          index = index % arr.length;
-        }
-        return arr[index];
-      },
-      getNextRandClass: function(){
-        var className = "nav-rand-hover-";
-        className += this.getNextRand();
-        //console.log(className);
-        return className;
+		return {
+			getRand: function(){
+				index += 1;
+				if (index >= arr.length){
+					index = index % arr.length;
+				}
+				return arr[index];
+			},
+			getNextNavClass: function(){
+				return (CLASS_NAMES.navHover + this.getRand());
+			},
+			getNextJumbotronClass: function(){
+				return (CLASS_NAMES.jumbo + this.getRand());
 			},
 			reshuffle: function(){
 				arr = shuffle(arr);
 			}
-    };
+		};
 
-    function shuffle(array) {
-      var currentIndex = array.length, temporaryValue, randomIndex;
+		function shuffle(array) {
+			var currentIndex = array.length, temporaryValue, randomIndex;
 
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
+			// While there remain elements to shuffle...
+			while (0 !== currentIndex) {
 
-        // Pick a remaining element...
-        var rand = new Date().getMilliseconds();
-        randomIndex = Math.floor(Math.random() * currentIndex );
-        currentIndex -= 1;
+				// Pick a remaining element...
+				var rand = new Date().getMilliseconds();
+				randomIndex = Math.floor(Math.random() * currentIndex );
+				currentIndex -= 1;
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
+				// And swap it with the current element.
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+			}
 
-      return array;
-    }
-  }
+			return array;
+		}
+	}
 
 }());
